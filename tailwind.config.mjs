76ea464922e8bdf0b2bcb4ab632b2ce28398d5a0
chark1es <charles15851@gmail.com>
2024-12-04 07:37:56 -0800

    add ieee colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            colors: {
+                ieee_navigation_bg: "#090d18",
+                ieee_online_store_bg: "#f3c135",
+                ieee_blue: "#00629b",
+            },
+        },
+    },
+    plugins: [],
+};
