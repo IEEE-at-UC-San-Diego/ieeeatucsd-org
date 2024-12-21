34539c7d51ef472d9fcaf64fcb9dacd184375c6f
chark1es <charles15851@gmail.com>
2024-12-21 03:24:29 -0800

    add color configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,12 +6,16 @@ export default {
       colors: {
         ieee_online_store_bg: "#f3c135",
         ieee_blue: "#00629b",
+        ieee_gold_text: "#ffbc00",
 
         nav_bg: "#090d19",
         nav_text: "#eaf7ff",
 
         bg_primary: "#0a0e1b",
         top_gradient: "#1c284e",
+
+        card_bg: "#0d1324",
+        card_text: "#eaf7ff",
       },
       backgroundImage: {
         "gradient-radial":
