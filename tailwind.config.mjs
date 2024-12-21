8d034ed451175e3be2cffcd3421d517130163ad0
chark1es <charles15851@gmail.com>
2024-12-21 15:35:19 -0800

    add colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,14 +8,25 @@ export default {
         ieee_blue: "#00629b",
         ieee_gold_text: "#ffbc00",
 
+        // Navigation colors
         nav_bg: "#090d19",
         nav_text: "#eaf7ff",
 
+        // Layout colors
         bg_primary: "#0a0e1b",
         top_gradient: "#1c284e",
 
+        // Main Page Card colors
         card_bg: "#0d1324",
         card_text: "#eaf7ff",
+        card_gradient: "#FFFFFF",
+
+        // Project Page Card colors
+        project_card_bg: "#0d1324",
+        project_card_text: "#FFFFFF",
+        project_card_gradient: "#000000",
+        project_button_bg: "#FFFFFF",
+        project_button_color: "#000000",
       },
       backgroundImage: {
         "gradient-radial":
@@ -23,5 +34,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-motion"), require("tailwindcss-animated")],
 };
