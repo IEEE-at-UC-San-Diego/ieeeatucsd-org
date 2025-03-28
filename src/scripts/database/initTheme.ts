d554272c724f198c31abc69be9b2f5f756e4c7c9
chark1es <charles15851@gmail.com>
2025-03-28 14:34:31 -0700

    Create initTheme.ts

---
+++ src/scripts/database/initTheme.ts
@@ -0,0 +1,7 @@
+import { initializeTheme } from "../../utils/themeUtils";
+
+/**
+ * Initialize theme settings from IndexedDB
+ * This function should be called on app startup
+ */
+export const initTheme = initializeTheme;
