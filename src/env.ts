99034a57b67409bbd33c013da176a8a94c59aab5
chark1es <charles15851@gmail.com>
2025-07-03 19:45:59 -0700

    add firebase

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,19 @@
 import { z } from "zod";
+interface ImportMetaEnv {
+  readonly FIREBASE_PRIVATE_KEY_ID: string;
+  readonly FIREBASE_PRIVATE_KEY: string;
+  readonly FIREBASE_PROJECT_ID: string;
+  readonly FIREBASE_CLIENT_EMAIL: string;
+  readonly FIREBASE_CLIENT_ID: string;
+  readonly FIREBASE_AUTH_URI: string;
+  readonly FIREBASE_TOKEN_URI: string;
+  readonly FIREBASE_AUTH_CERT_URL: string
+  readonly FIREBASE_CLIENT_CERT_URL: string;
+}
 
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
 /**
  * Define a schema for environment variables with Zod
  * This provides type safety and validation for environment variables
