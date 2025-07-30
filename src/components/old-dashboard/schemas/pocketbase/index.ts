53607080faeab22382763c9c915e3e3fdcf0eb77
chark1es <charles15851@gmail.com>
2025-07-30 01:24:16 -0700

    remove old dashboard

--- src/components/old-dashboard/schemas/pocketbase/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * PocketBase Schema Index
- *
- * This file re-exports all types and constants from the schema file
- * for easier imports throughout the codebase.
- */
-
-export * from "./schema";
